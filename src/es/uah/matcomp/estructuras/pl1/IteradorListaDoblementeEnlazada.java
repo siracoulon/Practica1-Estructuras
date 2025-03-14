0b2351274fd89f687c2775ecbe7268b11c9cb951
Sara-PzSZ <s.perezsanchez@edu.uah.es>
2025-03-14 11:21:18 +0100

    Correción

---
+++ src/es/uah/matcomp/estructuras/pl1/IteradorListaDoblementeEnlazada.java
@@ -0,0 +1,38 @@
+package es.uah.matcomp.estructuras.pl1;
+
+class IteradorListaDoblementeEnlazada<T> implements IteradorDoble<T> {
+    private ElementoDoble<T> actual;
+
+    public IteradorListaDoblementeEnlazada(ElementoDoble<T> inicio) {
+        this.actual = inicio;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return actual != null;
+    }
+
+    @Override
+    public T next() {
+        if (actual == null) {
+            throw new IllegalStateException("No hay más elementos.");
+        }
+        T dato = actual.dato;
+        actual = actual.siguiente;
+        return dato;
+    }
+
+    @Override
+    public boolean hasPrevious() {
+        return actual != null && actual.anterior != null;
+    }
+
+    @Override
+    public T previous() {
+        if (actual == null || actual.anterior == null) {
+            throw new IllegalStateException("No hay elementos anteriores.");
+        }
+        actual = actual.anterior;
+        return actual.dato;
+    }
+}
